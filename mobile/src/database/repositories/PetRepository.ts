e64a67cf5b5f3164747059a708cbbfb423fd2876
douglassql <douglasti.contato@gmail.com>
2026-02-04 20:42:21 -0300

    feat(database): adicionar repositório local para entidade pet

---
+++ mobile/src/database/repositories/PetRepository.ts
@@ -0,0 +1,15 @@
+import { db } from '../connection/database'
+import { Pet } from '../models/Pet'
+
+export const PetRepository = {
+  create(pet: Pet) {
+    db.runSync(
+      'INSERT INTO pets (name, birth_date, weight) VALUES (?, ?, ?)',
+      [pet.name, pet.birth_date, pet.weight]
+    )
+  },
+
+  findAll(): Pet[] {
+    return db.getAllSync('SELECT * FROM pets')
+  }
+}
