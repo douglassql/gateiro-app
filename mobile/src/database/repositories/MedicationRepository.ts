1fb0d17a7c14c709be358260a2067586f348157b
douglassql <douglasti.contato@gmail.com>
2026-02-04 21:03:08 -0300

    feat(database): adicionar repositório local para medicamentos

---
+++ mobile/src/database/repositories/MedicationRepository.ts
@@ -0,0 +1,24 @@
+import { db } from '../connection/database'
+import { Medication } from '@/features/medications/models/Medication'
+
+export const MedicationRepository = {
+  create(med: Medication) {
+    db.runSync(
+      `INSERT INTO medications 
+      (pet_id, name, dosage, start_date, next_dose_date, interval_hours)
+      VALUES (?, ?, ?, ?, ?, ?)`,
+      [
+        med.pet_id,
+        med.name,
+        med.dosage,
+        med.start_date,
+        med.next_dose_date,
+        med.interval_hours
+      ]
+    )
+  },
+
+  findAll(): Medication[] {
+    return db.getAllSync('SELECT * FROM medications')
+  }
+}
